f5101bbaffb5ec05a09a07d547b6853c3749ad79
SAN.D <das95sankaram@gmail.com>
2024-06-18 18:15:34 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Sidebar from "./components/Sidebar.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import {Route, Routes} from "react-router-dom";
+import FavouritesPage from "./pages/FavouritesPage.jsx";
+
+function App() {
+
+  return (
+      <div className='flex'>
+          <Sidebar />
+          <Routes>
+          <Route path='/' element={<HomePage />} />
+              <Route path='/favorites' element={<FavouritesPage />} />
+          </Routes>
+      </div>
+  )
+}
+
+export default App
